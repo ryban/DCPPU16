54bf021f0cf4339a11e1ac95885414eead322eb4
Ryan Van Gilder <rvangilder1@gmail.com>
2012-04-08 16:20:21 -0700

    first commit

---
+++ dcpu.h
@@ -0,0 +1,131 @@
+// Definitions for various aspects of the DCPU-16
+
+using namespace std;
+
+#include <sys/time.h>
+
+#define RAM_SIZE 0x10000
+#define NUM_REG 8
+#define CLOCK_SPEED 100000 // 100k Hz 
+
+// 0x8000 is the start of the terminal buffer
+// the terminal is 32 chars by 16 chars (32 columns, 16 rows)
+// this means 0x8000 - 0x81ff is the entire screen buffer
+// # + 48 = ascii representation for that digit
+// newline = 12
+
+
+#define SCREEN_BUFFER 0x8000
+#define TERMINAL_WIDTH 32
+#define TERMINAL_HEIGHT 16
+#define NUM_COLORS 0x1ff
+
+#define INPUT_BUFFER 0x9000
+// 0x9000 is the input buffer.
+// make sure to set 0x9000 to 0 when you are done with it, or don't want it
+// Many people impliment a 16 char ring buffer at 0x9000, I did 1 char. Easier to deal with
+
+
+#define NOB_ 0
+#define SET_ 1
+#define ADD_ 2
+#define SUB_ 3
+#define MUL_ 4
+#define DIV_ 5
+#define MOD_ 6
+#define SHL_ 7
+#define SHR_ 8
+#define AND_ 9
+#define BOR_ 10
+#define XOR_ 11
+#define IFE_ 12
+#define IFN_ 13
+#define IFG_ 14
+#define IFB_ 15
+
+
+// Map for what values point to what registers/addresses/literals
+#define REGISTER_LOW 0
+#define REGISTER_HIGH 7
+#define REGISTER_RAM_LOW 8
+#define REGISTER_RAM_HIGH 15
+#define NEXT_WORD_REG_LOW 16
+#define NEXT_WORD_REG_HIGH 23
+#define _POP 24
+#define _PEEK 25
+#define _PUSH 26
+#define _SP 27
+#define _PC 28
+#define _O 29
+#define NEXT_WORD 30
+#define NEXT_WORD_LITERAL 31
+#define LITERAL_VALUE_LOW 32
+#define LITERAL_VALUE_HIGH 63
+
+#define _A 0
+#define _B 1
+#define _C 2
+#define _X 3
+#define _Y 4
+#define _Z 5
+#define _I 6
+#define _J 7
+
+#ifndef DCPU_H
+#define DCPU_H
+
+class Dcpu
+{
+    public:
+        Dcpu(ifstream &code, bool debug, bool fast,int ms);
+        ~Dcpu();
+
+        void run();
+        void MemoryDump();
+	unsigned short *GetScreenBuffer();
+	void kill();
+        void PushInBuff(char c);
+    private:
+        bool DEBUG;
+        bool dont_kill;
+        int time_to_kill;
+        bool wait_cycles;
+        bool dirtyScreen;
+
+        unsigned short *registers;  // pointer to the registers, 8 total
+        unsigned short *RAM;        // pointer to the RAM, 0x10000 words total, 128K 
+        unsigned short PC;          // program counter, points to RAM address of current instruciton
+        unsigned short SP;          // points to the top of the stack
+        unsigned short O;           // register for overflow detection
+        unsigned short *literals;
+        bool skip_next_ins;
+        int cycles_to_wait;
+
+        void wait(int cycles, timeval &start);
+        void init_terminal();
+        void UpdateScreen();
+        bool OctetNonZero(int index);
+
+        unsigned short *GetValuePtr(int a); // gets a pointer to a value defined by an instruction
+
+/*0x0*/ void NonBasic(unsigned short _a, unsigned short _b);
+/*0x1*/ void SET(unsigned short _a, unsigned short _b); // a = b;
+/*0x2*/ void ADD(unsigned short _a, unsigned short _b); // a = a + b; O = 0x0001 if overflow, otherwise 0x0
+/*0x3*/ void SUB(unsigned short _a, unsigned short _b); // a = a - b; O = 0xfff is underflow, otherwise 0x0
+/*0x4*/ void MUL(unsigned short _a, unsigned short _b); // a = a * b; O = ((a * b) >> 16) & 0xffff
+/*0x5*/ void DIV(unsigned short _a, unsigned short _b); // a = a / b; O = ((a << 16) / b ) & 0xffff
+/*0x6*/ void MOD(unsigned short _a, unsigned short _b); // a = a % b; if b == 0, a = 0
+/*0x7*/ void SHL(unsigned short _a, unsigned short _b); // a = a << b; O = ((a << b)>>16) & 0xffff
+/*0x8*/ void SHR(unsigned short _a, unsigned short _b); // a = a >> b; O = ((a >> b)<<16) & 0xffff 
+/*0x9*/ void AND(unsigned short _a, unsigned short _b); // a = a & b;
+/*0xA*/ void BOR(unsigned short _a, unsigned short _b); // a = a | b;
+/*0xB*/ void XOR(unsigned short _a, unsigned short _b); // a = a ^ b;
+/*0xC*/ void IFE(unsigned short _a, unsigned short _b); // perform next instruction if a == b
+/*0xD*/ void IFN(unsigned short _a, unsigned short _b); // perform next instruction if a != b
+/*0xE*/ void IFG(unsigned short _a, unsigned short _b); // perform next instruction if a > b
+/*0xF*/ void IFB(unsigned short _a, unsigned short _b); // perform next instruction if (a & b) != 0
+};
+
+
+#endif
+
---
+++ emu.cpp
@@ -0,0 +1,215 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+#include "dcpu.h"
+
+bool valid_number_string(char *str)
+{
+    for(int c = 0; str[c]; c++)
+        if(!isdigit(str[c]))
+            return false;
+
+    return true;
+}
+void start(void *usr)
+{
+    // CPU thread
+    Dcpu *cpu = static_cast<Dcpu *> (usr);
+    cpu->run();
+}
+
+char ShiftChar(char c)
+{
+    if(isalpha(c))
+        return toupper(c);
+
+    // all if this does not work...
+    switch(c)
+    {
+        case '1': return '!';
+        case '2': return '@';
+        case '3': return '#';
+        case '4': return '$';
+        case '5': return '%';
+        case '6': return '^';
+        case '7': return '&';
+        case '8': return '*';
+        case '9': return '(';
+        case '0': return ')';
+        case '-': return '_';
+        case '=': return '+';
+        case '`': return '~';
+        case '[': return '{';
+        case ']': return '}';
+        case ';': return ':';
+        case '\'': return '"';
+        case ',': return '<';
+        case '.': return '>';
+        case '/': return '?';
+        case '\\': return '|';
+        default:
+            return c;
+    }
+}
+void AbnormalChar(sf::Event &Event, char &c)
+{
+    // SFML doesn't map directly to ascii
+    // so I got this...
+
+    if(Event.Key.Code == sf::Key::Return)
+        c = '\n';
+    else if(Event.Key.Code == sf::Key::Space)
+        c = ' ';
+    else if(Event.Key.Code == sf::Key::BackSlash)
+        c = '\\';
+    else if(Event.Key.Code == sf::Key::LBracket)
+        c = '[';
+    else if(Event.Key.Code == sf::Key::RBracket)
+        c = ']';
+    else if(Event.Key.Code == sf::Key::SemiColon)
+        c = ';';
+    else if(Event.Key.Code == sf::Key::Comma)
+        c = ',';
+    else if(Event.Key.Code == sf::Key::Period)
+        c = '.';
+    else if(Event.Key.Code == sf::Key::Quote)
+        c = '"';
+    else if(Event.Key.Code == sf::Key::Slash)
+        c = '/';
+    else if(Event.Key.Code == sf::Key::Tilde)
+        c = '`';
+    else if(Event.Key.Code == sf::Key::Equal)
+        c = '=';
+    else if(Event.Key.Code == sf::Key::Dash)
+        c = '-';
+    else if(Event.Key.Code == sf::Key::Tab)
+        c = '\t';
+    else if(Event.Key.Code == sf::Key::Delete)
+        c = 127;
+    else if(Event.Key.Code == sf::Key::Back)
+        c = 8;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2)
+    {
+        cerr << "usage: dcpu <flags> <filename>\n";
+        return 1;
+    }
+
+    bool debug = false;
+    bool fast = false;
+    int time_to_kill_ms = 0;
+
+    // -d runs in debug mode, prints out memory dump at end
+    // -t <value> is the time in ms until the program exits (not very useful anymore)
+    // -f will not perform cycle checks, i.e. ignore the wait(cycles) command (recomended because the timing is WAY off)
+
+    for(int a = 1; a < argc - 1; a++)
+    {
+        if(!strcmp(argv[a], "-d"))
+        {
+            debug = true;
+        }else if(!strcmp(argv[a], "-t"))
+        {
+            if(valid_number_string(argv[a+1]))
+                time_to_kill_ms = atoi(argv[a+1]);
+            else
+                cerr << "invalid command <-t " << argv[a+1] << ">. ignoring\n";
+            a++;                    // ignore the command after -t
+        }else if(!strcmp(argv[a], "-f"))
+        {
+            fast = true;
+        }else
+        {
+            cerr << "invalid command <" << argv[a] << ">. ignoring\n";
+        }
+    }
+
+    ifstream inFile(argv[argc - 1], ios::in);
+    if(!inFile.good())
+    {
+        cerr << "Could not open <" << argv[argc - 1] << ">\n";
+        return 1;
+    }
+
+    Dcpu cpu(inFile, debug, fast, time_to_kill_ms);
+
+    sf::Thread cpu_thread(&start, &cpu);
+
+    cpu_thread.Launch();
+
+    sf::RenderWindow app(sf::VideoMode(TERMINAL_WIDTH * 18 + 32, TERMINAL_HEIGHT * 32 + 20), "DCPPU: DCPU-16 Emulator");
+    bool running = true;
+
+    while(running)
+    {
+        sf::Event Event;
+        while(app.GetEvent(Event))
+        {
+            if(Event.Type == sf::Event::Closed)
+            {
+                cpu.kill();
+                running = false;
+                cpu_thread.Wait();
+                app.Close();
+            }
+            if(Event.Type == sf::Event::KeyPressed)
+            {
+                char c = Event.Key.Code;
+                AbnormalChar(Event, c);
+                if(Event.Key.Shift)
+                    cpu.PushInBuff(ShiftChar(c));
+                else
+                    cpu.PushInBuff(c);
+
+            }
+
+        }
+        app.Clear(sf::Color(56, 83, 255));
+
+        sf::String Text;
+        Text.SetSize(32);
+
+        unsigned short *buffer = cpu.GetScreenBuffer();
+
+        int row = 0;
+        int col = 0;
+
+        char *row_t = new char[TERMINAL_WIDTH+1];
+        row_t[TERMINAL_WIDTH] = 0;
+
+        for(int i = 0; i < TERMINAL_HEIGHT * TERMINAL_WIDTH; i++)
+        {
+            char c = buffer[i];
+            if(c == '\n' || col >= TERMINAL_WIDTH)
+            {
+                Text.SetText(row_t);
+                Text.SetPosition(15, row * 32 + 5);
+                app.Draw(Text);
+
+                col = 0;
+                row++;
+                if(row >= TERMINAL_HEIGHT)
+                    break;
+                continue;
+            }
+            if(c == 0 || c == '\t')
+            {
+                c = ' ';
+            }
+            row_t[col] = c;
+            col++;
+        }
+
+        app.Display();
+    }
+    
+    return 0;
+}
